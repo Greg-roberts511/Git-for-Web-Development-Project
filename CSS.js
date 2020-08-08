4df35b5dfd1f75b4cac7390a34f38e8aa4348921
Greg Roberts <gregaroberts511@gmail.com>
2020-08-07 17:53:53 -0700

    Added css to project files

---
+++ CSS.js
@@ -0,0 +1,36 @@
+body { 
+  border: 20px solid red;
+  font-size: 20px; 
+  margin: 8px;
+}
+
+div.container {
+    width: 80%;
+    margin: 0 auto;
+}
+
+header {
+  text-align: center;
+}
+
+div.info {
+    background-color: orange;
+    padding: 5%;
+}
+
+.fisrt_section {
+    background-color: yellow;
+    padding: 10%;
+}
+
+.second-section {
+    line-height: 1.5;
+    letter-spacing: 2px;
+}
+
+.images {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    padding: 10%;
+}
